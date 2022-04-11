27975aed2778a4522dca95172374d801e7f20a13
aarrbba123 <70095196+aarrbba123@users.noreply.github.com>
2022-04-11 20:23:23 +0800

    Readd modded content + fix #1
    This is actually going pretty good
    -Fixed a metric TON of build issues
    -Made it so that .bak files don't cause an  issue to us

--- src/simulation/Particle.h
+++ src/simulation/Particle.h
@@ -3,6 +3,7 @@
 #include "Config.h"
 
 #include <vector>
+#include "BioData.h"
 #include "StructProperty.h"
 
 struct Particle
@@ -16,6 +17,7 @@ struct Particle
 	int flags;
 	int tmp;
 	int tmp2;
+	BioData bio;
 	unsigned int dcolour;
 	/** Returns a list of properties, their type and offset within the structure that can be changed
 	 by higher-level processes referring to them by name such as Lua or the property tool **/
